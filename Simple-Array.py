09c7d982a3f5d3cded5c796b42d1a0f3abea0f20
ellenedaniel <35457058+ellenedaniel@users.noreply.github.com>
2018-04-13 08:49:26 +0800

    Add files via upload

---
+++ Simple-Array.py
@@ -0,0 +1,6 @@
+from array import *
+my_fruits = ["Mango", "Strawberry", "Peanut", "Watermelon", "Banana"]
+print(my_fruits[1:4])
+print(my_fruits[ : 3])
+print(my_fruits[-4:])
+print(my_fruits[-3:-1])
